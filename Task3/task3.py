d3647a412338ef1dc9ec5f8df52e31d91d6c2c2e
Khametov Bakir <bakir_90@inbox.ru>
2022-12-20 10:36:10 +0300

    add task3

---
+++ Task3/task3.py
@@ -0,0 +1,12 @@
+# Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
+# Пример:
+# - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
+
+n = int(input('Enter number: ')) 
+dict = []
+for i in range (1, n + 1):
+    dict.append ([round((1 + 1 / i)**i, 2)])
+        #sum = sum + dict     
+print(dict, end=" ")
+#print(sequence())
+   
