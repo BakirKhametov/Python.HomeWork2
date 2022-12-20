86501f3df7487677faca259327fb78addb205a23
Khametov Bakir <bakir_90@inbox.ru>
2022-12-20 10:47:53 +0300

    second change task3

--- Task3/task3.py
+++ Task3/task3.py
@@ -2,7 +2,7 @@
 # Пример:
 # - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
-n = int(input('Enter number: ')) 
+n = int(input('Введите число: ')) 
 dict = []
 for i in range (1, n + 1):
     dict.append ([round((1 + 1 / i)**i, 2)])   
